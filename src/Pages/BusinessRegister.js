d96c775c5289960f979414dc5f66fbd6906c29b7
raithathajaydip <jaydipraithatha26@gmail.com>
2023-12-14 13:49:50 +0530

    Business Register Done

--- src/Pages/BusinessRegister.js
+++ src/Pages/BusinessRegister.js
@@ -1,34 +1,103 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
+import swal from "sweetalert";
+import URL from '../helper/url';
+
 
 const BusinessRegister = () => {
+    // Hooks Area
+        const [cities,setCities] =useState([]);
+        const [businessCategories,SetBusinessCategories] =useState([]);
+       
+        useEffect(()=>{
+            //call the city api
+            fetch(`${URL}/api/cities`,{})
+            .then((res)=>{
+                    return res.json()
+            })
+            .then((citydata)=>{
+                    console.log("Citydata",citydata)
+                    setCities(citydata.data);
+            })
+            .catch((error)=>{
+                return error;
+            })
+                //call the business category api
+                fetch(`${URL}/api/business-categories`,{})
+                .then((res)=>{
+                    return res.json()
+                })
+                .then((categorydata)=>{
+                        console.log("Categorydata",categorydata);
+                         SetBusinessCategories(categorydata.data);
+                })
+                .catch((error)=>{
+                    return error;
+                })
+        },[])
+
+            const busReg = (event) => {
+                event.preventDefault();
+               // alert("Okkkk");   
+                let payload ={
+                                "data": {
+                                "name":document.querySelector('input[name="business_name"]').value,
+                                "business_category":document.querySelector('select[name="bus_cat_id"]').value,
+                                "cities": [
+                                    document.querySelector('select[name="city_id"]').value,
+                                ]
+                                }
+                  };
+                  // call the Api
+
+                  console.log(payload);
+                    fetch(`http://localhost:1337/api/businesses`,{
+                    method:'POST',
+                    headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify(payload),
+                    })
+                    .then((res)=>{
+                       return res.json();
+                    })
+                    .then((data)=>{
+                         console.log("Business Register",data);
+                        swal("Good job!", "Business Registerd Successfully..!", "success");
+                })
+                .catch((error)=>{
+                    console.log(error);
+                })
+            }           
   return (
     <>
         <h1 className='text-center'>Business Register</h1>
              <Form className='col-sm-6 offset-3'>
                     <Form.Group className="mb-3">
                         <Form.Label>City</Form.Label>
-                        <Form.Select aria-label="Default select example">
-                        <option>Select City</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <Form.Select name="city_id" aria-label="Default select example">
+                            {
+                                cities.map((cv,idx,arr)=>{
+                                    return <option key={idx} id={cv.id}>{cv.attributes.name}</option>
+                                })
+                            }
                         </Form.Select>
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>Business Category</Form.Label>
-                        <Form.Select aria-label="Default select example">
-                        <option>Select Business Category</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        <Form.Select name="bus_cat_id" aria-label="Default select example">
+                            {
+                                businessCategories.map((cv,idx,arr)=>{
+                                    return <option key={idx} id={cv.id}>{cv.attributes.Name}</option>
+                                })
+                            }
                         </Form.Select>
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>Business Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Business Name" />
+                        <Form.Control type="text" name="business_name" placeholder="Enter Business Name" />
                     </Form.Group>
-                    <Button variant="primary" type="button" onClick={()=>{}}>
+                    <Button variant="primary" type="submit" onClick={(event)=>{busReg(event)}}>
                         Register Business
                     </Button>
             </Form>
--- src/component/ui/Navigation.js
+++ src/component/ui/Navigation.js
@@ -1,60 +1,69 @@
-import React from 'react'
-import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap'
-import  Logo from "../../logo.svg";
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+import Logo from "../../logo.svg";
+import { Link } from "react-router-dom";
 export default function Navigation() {
-
   const myLogout = () => {
-     window.localStorage.removeItem('jwttoken')
-    window.location.href='/login';
-  }
+    window.localStorage.removeItem("jwttoken");
+    window.location.href = "/login";
+  };
   return (
     <>
-        <Navbar expand="lg" className="bg-body-tertiary h-100">
-            <Container fluid>
-              <Navbar.Brand href="#">
-                  <img
-                  src={Logo}
-                  width="100"
-                  className="d-inline-block align-top"
-                  alt="React Bootstrap logo"
-                 />
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="navbarScroll" />
-              <Navbar.Collapse id="navbarScroll">
-                <Nav
-                  className="me-auto my-2 my-lg-0"
-                  style={{ maxHeight: '100px' }}
-                  navbarScroll
-                >
-                  
-                  <Link to="/" className='nav-link'>Home</Link>
-                  {
-                    window.localStorage.getItem('jwttoken') === null &&
-                    <>
-                    <Link to="/login" className='nav-link'>Login</Link>
-                    <Link to="/register" className='nav-link'>Register</Link>
-                    </>
-                  }
-                  {
-                     window.localStorage.getItem('jwttoken') !== null &&
-                    <>
-                    <Nav.Link onClick={()=>{myLogout()}} className='nav-link'>Logout</Nav.Link>
-                    </>
-                  }
-                </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </Navbar.Collapse>
-            </Container>
-        </Navbar>
+      <Navbar expand="lg" className="bg-body-tertiary h-100">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            <img
+              src={Logo}
+              width="100"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+              {window.localStorage.getItem("jwttoken") === null && (
+                <>
+                  <Link to="/login" className="nav-link">
+                    Login
+                  </Link>
+                  <Link to="/register" className="nav-link">
+                    Register
+                  </Link>
+                </>
+              )}
+              {window.localStorage.getItem("jwttoken") !== null && (
+                <>
+                  <Nav.Link
+                    onClick={() => {
+                      myLogout();
+                    }}
+                    className="nav-link"
+                  >
+                    Logout
+                  </Nav.Link>
+                </>
+              )}
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
-  )
+  );
 }
